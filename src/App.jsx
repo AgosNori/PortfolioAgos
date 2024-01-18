e3efecce5d172d14d4b123aa8f52f8922bef3dee
agos <agosnoriega2018@gmail.com>
2024-01-18 00:18:19 -0300

    responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import AboutMe from "./Components/AboutMe/AboutMe";
 import Banner from "./Components/Banner/Banner";
 import Contacto from "./Components/Contacto/Contacto";
 import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
+
 import Proyectos from "./Components/Proyectos/Proyectos";
 import Redes from "./Components/RedesSociales/Redes";
 import Skills from "./Components/Skills/Skills";
 import './App.css';
 import Loader from "./Components/Loader/Loader";
 
+import Header from './Components/Header/Header';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -17,7 +18,7 @@ function App() {
     // Simula un tiempo de carga de 10 segundos
     const loadingTimeout = setTimeout(() => {
       setIsLoading(false);
-    },2500);
+    }, 2500);
 
     // Limpia el timeout al desmontar el componente
     return () => clearTimeout(loadingTimeout);
@@ -29,7 +30,7 @@ function App() {
         <Loader />
       ) : (
         <>
-          <Header />
+          <Header/>
           <Redes />
           <Banner />
           <AboutMe />
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,26 +1,58 @@
+import { useState, useEffect } from 'react';
 import './Header.css';
+
 function Header() {
-    return (
-        <div id="header">
-            <h2>Agostina Noriega </h2>
-            <ul className="nav">
-                <li>
-                    <a href="#App">Home</a>
-                </li>
-                <li>
-                    <a href="#proyectos">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#sobremi">Sobre Mi</a>
-                </li>
-                <li>
-                    <a href="#contacto">Contacto</a>
-                </li>
-                <li>
-                    <a href="./CvNoriegaActualizado (2).pdf">CV</a>
-                </li>
-            </ul>
-        </div>
-    )
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Limpia el listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div id="header">
+      <div className='titulo'>
+        <h2>Agostina Noriega</h2>
+      </div>
+      <nav className='barra'>
+        {isMobile && (
+          <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+            &#9776;
+          </div>
+        )}
+        <ul className={`nav ${showMobileMenu ? 'show' : ''}`}>
+          <li>
+            <a href="#App">Home</a>
+          </li>
+          <li>
+            <a href="#proyectos">Portfolio</a>
+          </li>
+          <li>
+            <a href="#sobremi">Sobre Mi</a>
+          </li>
+          <li>
+            <a href="#contacto">Contacto</a>
+          </li>
+          <li>
+            <a href="./CvNoriegaActualizado (2).pdf">CV</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
-export default Header
+
+export default Header;
