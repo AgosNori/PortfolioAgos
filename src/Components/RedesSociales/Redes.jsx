e699a345307c8ff85c30b43c16eb3a3f16405251
agos <agosnoriega2018@gmail.com>
2024-01-18 00:35:00 -0300

    responsive redes

--- src/Components/RedesSociales/Redes.jsx
+++ src/Components/RedesSociales/Redes.jsx
@@ -2,7 +2,7 @@ import './Redes.css';
 function Redes(){
     return(
         <div className="redes">
-            <ul>
+            <ul className='item'>
                 <li><a href="https://github.com/AgosNori"><img src="./github-logo_icon-icons.com_73546.png" alt="" /></a></li>
                 <li><a href="https://www.instagram.com/"><img src="./Instagram_New_icon-icons.com_69008.png" alt="" /></a></li>
                 <li><a href="https://www.linkedin.com/in/agostina-gisel-noriega-21b137202/"><img src="./sociallinkedin_member_70.png" alt="" /></a></li>
