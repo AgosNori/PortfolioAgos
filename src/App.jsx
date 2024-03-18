30ab332e861cae3145e9ff7cd99ba22f28bcdc2d
agos <agosnoriega2018@gmail.com>
2024-03-17 22:02:02 -0300

    logica del formulario de contacto

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <Loader />
       ) : (
         <>
-          <Header/>
+          <Header />
           <Redes />
           <Banner />
           <AboutMe />
--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -1,21 +1,52 @@
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 import './Contacto.css';
 
-function Contacto() {
+export const Contacto = () => {
+    const refForm = useRef()
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //console.log(refForm.current)
+        const serviceId = 'service_iabk8ro';
+        const templateId = 'template_ya6emoa';
+        const apikey = 'tmaJSOIUwGV7FdVsE';
+
+        emailjs.sendForm(serviceId, templateId, refForm.current, apikey)
+
+            .then(result => console.log(result.text))
+            .catch(error => console.error(error))
+
+    }
+
     return (
         <div className='form'>
-            <form className='formulario'>
+            <form className='formulario' ref={refForm} action="" onSubmit={handleSubmit}>
                 <h2>Contáctame</h2>
-                <input type="email" placeholder="Ingrese su email" />
-                <label className='email' type="email" ></label>
-                <input type="text" placeholder="Ingrese su nombre completo" />
-                <label className='name' type="text" ></label>
-                <input type="number" placeholder='Ingrese su numero de telefono' />
-                <label className='number' type="number"></label>
-                <textarea className='texto' name="dudas" cols="20" rows="6" placeholder='Deje su mensaje'></textarea>
+                <fieldset>
+                    {/* <label className=' email' type="email" name="email">Email</label>
+                    <input type="email" placeholder="Ingrese su email" required /> */}
+                    <label className="symbol-required email" htmlFor="">Email</label>
+                    <input name="email" type="email" placeholder="Ingresa tu email" required />
+                </fieldset>
+                <fieldset>
+                    {/* <label className=' name' type="text" name="name" >Nombre Completo</label>
+                    <input type="text" placeholder="Ingrese su nombre completo" required /> */}
+                    <label className="symbol-required name" htmlFor="">Name</label>
+                    <input name="name" type="text" placeholder="Ingresa tu nombre" required />
+                </fieldset>
+                <fieldset>
+                    <label className='symbol-required number' type="number" htmlFor="" >Numero de telefono</label>
+                    <input type="number" name="number" placeholder='Ingrese su numero de telefono' />
+                </fieldset>
+                <fieldset>
+                    <label className="symbol-required name" htmlFor="">Consulta</label>
+                    <textarea className='texto' name="message" cols="20" rows="6" placeholder='Deje su mensaje'></textarea>
+                </fieldset>
+
                 <button className='btn'>Enviar</button>
             </form>
         </div>
 
     )
 }
-export default Contacto;
+export default Contacto
