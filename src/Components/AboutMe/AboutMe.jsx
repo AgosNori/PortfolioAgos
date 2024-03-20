9de20df6db61c76274653c39edc89e6e8f3ae63e
agos <agosnoriega2018@gmail.com>
2024-03-20 03:01:32 -0300

    actualizando los datos de los proyectos

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,12 +1,13 @@
 import './AboutMe.css';
 function AboutMe() {
     return (
-        
+
         <div id='sobremi' className="aboutme">
             <h2>Un poco sobre mi </h2>
-            <p>Soy desarrolladora Full Stack, pero me gusta mas el frontend , me encanta codear. Siempre me encuentro realizando alguna capacitación, estoy cursando Ingenieria Electromécanica en la UTN y trabajo dictando clases de programación en diferentes escuelas de mi ciudad.
+            <p>Soy desarrolladora Full Stack, pero me gusta mas el frontend , me encanta codear. Siempre me encuentro realizando alguna capacitación, estoy cursando Ingenieria Electromécanica en la UTN. Me encuentro dictando clases de python en el Centro de fornación profesional del politecnico Malvinas Argentinas y he comenzado a trabajar en la Agencia de Innovación TDF como desarrolladora web.
+                Estoy realizando una pasantia dentro de la Usina de talentos IT con la fundación Mirgor de DevOps
             </p>
-            <a href="#contacto"><button  className="btn">Contáctame</button></a>
+            <a href="#contacto"><button className="btn">Contáctame</button></a>
         </div>
     )
 }
--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -8,7 +8,7 @@ const proyectosData = [
     imagen: './depapa.png',
     alt: 'DePapa-Front Image',
     titulo: 'DePapa Restaurante',
-    descripcion: 'Esta pagina esta diseñada para un restaurante, esta creada con HTML y CSS',
+    descripcion: 'Esta pagina presenta un catálogo completo de las recetas exclusivas del restaurante, detallando paso a paso cómo preparar cada plato de manera fácil y comprensible. Cada receta está meticulosamente explicada para que los usuarios puedan replicar los deliciosos sabores en la comodidad de sus hogares. Herramientas utilizadas para la creción de la misma: HTML, CSS y Bootstrap.',
     video: './dePapa.mp4'
 
   },
@@ -17,7 +17,7 @@ const proyectosData = [
     imagen: './CrudReact (1).png',
     alt: 'CrudReact Image',
     titulo: 'CrudReact',
-    descripcion: 'Esta pagina ha sido creada mediante un CRUD con React Js',
+    descripcion: 'Este proyecto es un sistema de gestión de usuarios desarrollado utilizando React.js, una biblioteca de JavaScript de código abierto ampliamente utilizada para la construcción de interfaces de usuario interactivas. El sistema ofrece operaciones CRUD completas (Crear, Leer, Actualizar, Eliminar) para administrar una base de datos de usuarios de manera eficiente. Herramientas utilizadas para la creación de la misma: React Js',
     video: './crudReact.mp4'
   },
   {
@@ -25,39 +25,39 @@ const proyectosData = [
     imagen: './ProyRickMorty.png',
     alt: 'ProyectReact Image',
     titulo: 'Consumiendo API',
-    descripcion: 'Esta pagina esta consumiendo una API, realizada con React Js',
+    descripcion: 'Este proyecto es un explorador de personajes de la famosa serie "Rick and Morty", desarrollado utilizando React.js. Utiliza una API pública que proporciona información detallada sobre los personajes de la serie, incluyendo nombres, imágenes, especies, géneros, origen, ubicación actual y más.Herramientas utilizadas para la creación de la misma: React Js',
     video: './reactRickMorty.mp4'
   },
   {
     url: 'https://agosnori.github.io/MyPerfum/',
     imagen: './perfumeria.png',
     alt: 'MyPerfum Image',
-    titulo: 'DePapa Restaurante',
-    descripcion: 'Esta pagina estad diseñada para una perfumeria, realizada con HTML y CSS',
+    titulo: 'MyPerfum',
+    descripcion: 'Esta página web es el catálogo en línea de la perfumería "My Perfum", donde los clientes pueden explorar una amplia selección de perfumes y fragancias de alta calidad. Herramientas utilizadas para la creación de la misma: HTML y CSS.',
     video: './Myperfum.mp4'
   },
   {
     url: 'https://agosnori.github.io/TimerForCubbing/',
     imagen: './timer.png',
     alt: 'TimerForCubbing Image',
-    titulo: 'DePapa Restaurante',
-    descripcion: 'Proyecto de un Timmer para Cubbing , creada con HTML, CSS y JS',
+    titulo: 'Timmer',
+    descripcion: 'Este proyecto consiste en un temporizador de cubing. El temporizador permite a los participantes cronometrar el tiempo que tardan en resolver el cubo, lo que les ayuda a medir su progreso y mejorar su habilidad en la resolución del cubo. Herramientas utilizadas para la creación del mismo: HTML, CSS y Js.',
     video: './TimmerForCubbing.mp4'
   },
   {
     url: 'https://github.com/AgosNori/react15-11',
     imagen: './crud.png',
     alt: 'React15-11 Image',
-    titulo: 'DePapa Restaurante',
-    descripcion: 'Proyecto de una biblioteca virtual , trabajando con CRUD y realizado con React Js',
+    titulo: 'Biblioteca Virtual',
+    descripcion: 'Este proyecto es una biblioteca virtual desarrollada con React.js que permite a los usuarios gestionar una colección de libros de manera eficiente. La aplicación ofrece operaciones CRUD completas (Crear, Leer, Actualizar, Eliminar) para administrar la información de los libros de manera dinámica.Herramientas utilizadas para la creación del mismo: React Js',
     video: './biblioReact.mp4'
   },
   {
     url: 'https://agosnori.github.io/Vet/',
     imagen: './vet.png',
     alt: 'Vet Image',
-    titulo: 'DePapa Restaurante',
-    descripcion: 'Esta pagina fue realizada para una veterinaria, con HTML y CSS',
+    titulo: 'Veterinaria Huellitas',
+    descripcion: 'Esta página web es un portal en línea para la clínica veterinaria "Huellitas", donde los usuarios pueden explorar y adoptar a adorables mascotas que están buscando un hogar amoroso. La página tiene como objetivo facilitar el proceso de adopción y promover la adopción responsable de animales. Herramientas utilzadas para la creación de la misma: HTML y CSS.',
     video: ''
   },
   {
@@ -65,15 +65,15 @@ const proyectosData = [
     imagen: './ecommerce.png',
     alt: 'TPFinalFrontend Image',
     titulo: 'DePapa Restaurante',
-    descripcion: 'Trabajo final de la diplomatura de desarrollo full stack, React',
+    descripcion: 'Goddesses es un eCommerce especializado en la venta de productos para el cuidado del cabello, la piel y la cara, ofreciendo una amplia gama de productos naturales y organicos para el cuidado y la salud.Este proyecto fue el culmen de mi Diplomatura en desarrollo Full Stack. Herramientas Utilizadas para la creación del mismo: React Js, MySql y Ejs',
     video: './TpFinalDiplo.mp4'
   },
   {
     url: 'https://github.com/AgosNori/JuegoDicFinal',
     imagen: './juegojs.png',
     alt: 'JuegoDicFinal Image',
-    titulo: 'DePapa Restaurante',
-    descripcion: 'Juego ahorcado, creado con HTML, CSS y Js',
+    titulo: 'Juego Ahorcado',
+    descripcion: 'El juego del ahorcado es un clásico juego de palabras donde los jugadores intentan adivinar una palabra oculta, letra por letra, antes de que se complete la figura de un hombre ahorcado. Herramientas utilizada para la creación del mismo: HTML, CSS y Js.',
     video: './juegoAhorcado.mp4'
   },
   {
@@ -81,7 +81,7 @@ const proyectosData = [
     imagen:'./todolist.png',
     alt:'TodoListDjango Image',
     titulo:'To do List With Django',
-    descripcion:'Lista de tareas realizadas con Django',
+    descripcion:'Esta To-Do List es una aplicación web desarrollada con Django, un framework de desarrollo web en Python. La aplicación permite a los usuarios gestionar sus tareas de manera eficiente, creando, editando, completando y eliminando tareas según sea necesario.Herramientas utilizadas para la creación del mismo: Django',
     video:''
   },
   {
@@ -89,7 +89,7 @@ const proyectosData = [
     imagen:'./dino.png',
     alt:'JuegoDino Image',
     titulo:' Dino--SinConex-n',
-    descripcion:'Juego del dino de google hecho con HTML, CSS y JS',
+    descripcion:'En este proyecto hemos recreado el juego del dinosaurio de Google. Herramientas utilizadas para la creación del mismo: HTML, CSS y Js.',
     video:''
   },
 ];
